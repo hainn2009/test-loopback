ae09d40a1aef838a947d78f56e4c4b36ddd4f9d3
Nguyen Ngoc Hai <hainn2009@gmail.com>
2021-12-27 09:55:27 +0700

    Add createMeetUp method

--- common/models/meetup.js
+++ common/models/meetup.js
@@ -1,5 +1,13 @@
-'use strict';
+"use strict";
 
-module.exports = function(Meetup) {
-
+module.exports = function (Meetup) {
+  Meetup.createMeetUp = async (meetUp) => {
+    try {
+      const response = await Meetup.create(meetUp);
+      return response;
+    } catch (err) {
+      console.log(err);
+      return err;
+    }
+  };
 };
---
+++ server/boot/create-table-new.js
@@ -0,0 +1,5 @@
+// module.exports = function (app) {
+//   app.dataSources.postgres.automigrate("meetup", function (err) {
+//     if (err) throw err;
+//   });
+// };
